fcb7f1e7574e57bc28c922bc47acfd82811cd459
Мокроусов Михаил Александрович <ip815s14@lwp05.csc.local>
2019-03-13 12:13:10 +0700

    Ready project

--- geom.cpp
+++ geom.cpp
@@ -1,13 +1,30 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main(){
-	char a[100],b[100];
-	int i;
-	for(i=0;i<100;i++){
-		if(a[i]=="(") break;
-		b[i]=a[i];
-	}
-	scanf("%s",&a);
-	printf("%s",b);
-	return 0;
+#include <cstring>
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    char a[100], b[100], emp[] = "\0";
+    int i;
+    while (1) {
+        scanf("%s", a);
+
+        if (strstr(a, "exit"))
+            break;
+        if (strstr(a, "circle"))
+            printf("circle\n");
+        else
+            printf("unknown shape %s \n", a);
+
+        //      for (i = 0; i < 100; i++) {
+        //            if (a[i] == '(') {
+        // strncat(b, emp, 2);
+
+        //            break;
+        //      }
+        //    b[i] = a[i];
+        //}
+        // printf("%s \n", b);
+        // strcpy(b, emp);
+    }
+    return 0;
 }
