3d73c94334d1086b6286572c31d9873626f89220
Мокроусов Михаил Александрович <ip815s14@lwp05.csc.local>
2019-03-20 12:38:12 +0700

    Little upgrade of input

--- geom.cpp
+++ geom.cpp
@@ -5,35 +5,15 @@ int main()
 {
     char a[100], b[100], emp[] = "\0";
     int i;
-    while (1) {
-        scanf("%s", a);
-
-        if (strstr(a, "exit"))
+    scanf("%s", a);
+    for (i = 0; i < 100; i++) {
+        if (a[i] == '(')
             break;
-        if (strstr(a, "circle")) {
-            printf("%s\n", a);
-            //            for(i = 0; i < 100; i++) {
-            //              if (a[i] == '(') {
-            //                strncat(b, emp, 2);
-            //
-            //                  break;
-            //            }
-            //          b[i] = a[i];
-            //    }
-
-        } else
-            printf("unknown shape %s \n", a);
-
-        for (i = 0; i < 100; i++) {
-            if (a[i] == '(') {
-                strncat(b, emp, 2);
-
-                break;
-            }
-            b[i] = a[i];
-        }
-        // printf("%s \n", b);
-        // strcpy(b, emp);
+        b[i] = a[i];
     }
+    strcat(b, emp);
+    if (strcmp(b, "circle") == 0)
+        printf("%s", b);
+
     return 0;
 }
